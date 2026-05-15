b2211bb2ee50d9d30c19c550e2ec87efb8ff9566
Ahnji1 <ahnji.ahn1@nhs.net>
2026-05-15 12:18:29 +0000

    Set start page as homepage

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', function (req, res) {
+  res.render('start-page')
+})
+
 module.exports = router
